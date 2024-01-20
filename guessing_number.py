13f3b7b5a360fa9e55d53f5e0880582e037d0988
zyniu81@wp.pl <zyniu81>
2024-01-20 19:23:18 +0100

    Second version

--- guessing_number.py
+++ guessing_number.py
@@ -5,6 +5,8 @@ def game(n=10):
     print(f"""Think the number from 0 to 1000 and I will guess it as quickly as possible. {n} attempts."
           ENTER Start""")
     list_computer = []
+    list_computer_min = []
+    list_computer_max = []
     while len(list_computer) < n:
         user_input = input()
         if user_input == "Start":
@@ -13,18 +15,18 @@ def game(n=10):
             print(list_computer)
             continue
         if user_input == "Too small":
-            random_min = list_computer[-1] + 1
-            random_max = 1000
+            random_min = max(list_computer_min, default=0)
+            random_max = max(list_computer_max, default=1000)
             computer = random.randint(random_min, random_max)
-            list_computer.append(computer)
-            print(list_computer)
+            list_computer_min.append(computer)
+            print(list_computer_min + list_computer_max)
             continue
         if user_input == "Too big":
-            random_min = 0
-            random_max = list_computer[-1] - 1
+            random_min = max(list_computer_min, default=0)
+            random_max = max(list_computer_max, default=1000)
             computer = random.randint(random_min, random_max)
-            list_computer.append(computer)
-            print(list_computer)
+            list_computer_max.append(computer)
+            print(list_computer_min + list_computer_max)
             continue
         if user_input == "You win":
             print("Thank you for the game")
