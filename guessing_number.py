ebac42e32334345da06bb69082164e020b3ab197
zyniu81@wp.pl <zyniu81>
2024-01-21 12:46:44 +0100

    Third version

--- guessing_number.py
+++ guessing_number.py
@@ -4,39 +4,40 @@ import random
 def game(n=10):
     print(f"""Think the number from 0 to 1000 and I will guess it as quickly as possible. {n} attempts."
           ENTER Start""")
-    list_computer = []
-    list_computer_min = []
-    list_computer_max = []
-    while len(list_computer) < n:
-        user_input = input()
-        if user_input == "Start":
-            computer = random.randint(0, 1000)
+    user_input = input()
+    if user_input == "Start":
+        pass
+    try:
+        min_number = 0
+        max_number = 1000
+        list_computer = []
+        while len(list_computer) <= n * 2 - 2:
+            computer = random.randint(min_number, max_number)
             list_computer.append(computer)
-            print(list_computer)
-            continue
-        if user_input == "Too small":
-            random_min = max(list_computer_min, default=0)
-            random_max = max(list_computer_max, default=1000)
-            computer = random.randint(random_min, random_max)
-            list_computer_min.append(computer)
-            print(list_computer_min + list_computer_max)
-            continue
-        if user_input == "Too big":
-            random_min = max(list_computer_min, default=0)
-            random_max = max(list_computer_max, default=1000)
-            computer = random.randint(random_min, random_max)
-            list_computer_max.append(computer)
-            print(list_computer_min + list_computer_max)
-            continue
-        if user_input == "You win":
-            print("Thank you for the game")
-            print("I'm win!")
-            break
-        if user_input == "Stop":
-            break
-        else:
-            print("I'm sorry. I'm not understand")
-            continue
+            print(computer)
+            user_input = input()
+            if user_input == "Too small":
+                min_number = computer + 1
+                list_computer.append(min_number)
+            elif user_input == "Too big":
+                max_number = computer - 1
+                list_computer.append(max_number)
+            elif user_input == "You win":
+                print(f"I'm win in {len(list_computer) / 2 + 0.5} attempt")
+                break
+            elif user_input == "Stop":
+                print("Interrupted by user")
+                break
+            else:
+                print("I didn't understand")
+            if len(list_computer) == n * 2:
+                print(list_computer)
+                print("I lost game")
+                break
+    except ValueError:
+        print("You cheating.")
+    except KeyboardInterrupt:
+        print("Interrupted by user")
 
 
 game()
